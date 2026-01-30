e6df86ed7f24c1b45d6084d8ecfc520704c6db36
Daniel <daniel.olivqueiroz@outlook.com>
2026-01-30 18:12:17 -0300

    feat: add counter functionality to About and HomePage components

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,8 @@
 <template>
   <h1>About</h1>
+  {{ counter }}
 </template>
+
+<script lang="ts" setup>
+const counter = useState("count", () => 0);
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <h1 class="text-4xl font-black">HomePage</h1>
   <NuxtLink href="/about">About</NuxtLink>
+  {{ counter }}
+  <button @click="counter++">click</button>
 </template>
 
 <script lang="ts" setup>
+const counter = useState<number>("count", () => 0);
+
+counter.value;
 useHead({
   title: "My Awesome Website",
 });
